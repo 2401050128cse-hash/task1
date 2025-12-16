1555d7e39952529228666cde55224b14407d85a8
Sri Sai Vignesh Kadiyala <2401050128cse@gmail.com>
2025-12-16 10:51:49 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,16 @@
+# Personal Introduction Program
+# Week 1: Python Basics
+
+print(" Welcome to the Personal Introduction Program!")
+print("Anitha is a passionate and aspiring software developer with a strong interest in Python programming and continuous learning.She enjoys building simple, practical projects to strengthen her understanding of core programming concepts and problem-solving skills.Anitha is focused on writing clean, readable code and is steadily developing her knowledge of software development practices and version control using Git and GitHub, with the goal of growing into a confident and skilled developer.")
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+city = input("Which city are you from? ")
+hobby = input("What is your favorite hobby? ")
+
+print("\nWelcome " + name + " Anitha")
+print("You are", age, "21")
+print("You live in", city + "Hyderabad")
+print("You love", hobby + "Software Job")
+print("\nThank you for sharing your details!")
